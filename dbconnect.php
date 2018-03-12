ccc3839be998d537007b73ef81737b0fb6c99617
秀和 <hidekazu5200@gmail.com>
2018-03-12 11:38:35 +0800

    初回コミット

---
+++ dbconnect.php
@@ -0,0 +1,10 @@
+
+ <?php  $dsn = 'mysql:dbname=seed_sns;host=localhost';
+ // XAMPP環境下ではユーザー名rootパスワード空
+ $user = 'root';
+ $password = '';
+ //このプログラムが存在している場所と同じサーバーを指定
+ $dbh = new PDO($dsn,$user,$password);
+ $dbh->query('SET NAMES utf8');
+ ?>
+ 
---
+++ logout.php
@@ -0,0 +1,20 @@
+<?php 
+ session_start();
+
+ $_SESSION = array();
+
+ if(ini_get('session.use_cookies')){
+   $params = session_get_cookie_params();
+   setcookie(session_name(), '',time() - 4200, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+ }
+
+ session_destroy();
+  setcookie('email', '', time()-3000);
+  setcookie('password', '', time()-3000);
+
+  header('Location: index.php');
+  exit;
+
+
+
+ ?>
